2547bf3c7476eae1743106a0de03d6097f189f56
Mark Long <mark.long@marklong.io>
2018-10-19 21:19:29 -0600

    Added players array made players selectable

--- js/myApp.js
+++ js/myApp.js
@@ -46,10 +46,18 @@ for(let i = 0; i < enemies; i++){
 // This class requires an update(), render() and
 // a handleInput() method.
 class Player {
-    constructor(px,py,sprite){
+    constructor(px,py){
       this.x = px;
       this.y = py;
-      this.sprite = sprite;
+      this.players = [
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png'
+      ];
+      this.selectedSprite = 0;
+      this.sprite = this.players[this.selectedSprite];  
     }
     update() {
       if(this.y <= 0){
@@ -62,6 +70,10 @@ class Player {
       ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
     handleInput(e) {
+      if (e === "players") {
+        (this.selectedSprite < 4) ? this.selectedSprite++ : this.selectedSprite = 0; 
+        player.sprite = this.players[this.selectedSprite];
+      }
       if (e === "left" && this.x >= 25) {
         this.x -= 25;
       }
@@ -77,12 +89,13 @@ class Player {
     }
   }
   // Place the player object in a variable called player
-  let player = new Player(200, 400, 'images/char-boy.png')
+  let player = new Player(200, 400)
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keyup', function(e) {
     var allowedKeys = {
+        16: 'players',
         37: 'left',
         38: 'up',
         39: 'right',
